2c42733f94e9c5d2d50de597ddf491f3830c6b99
Rubem Chaves <108533543+vinicchaves@users.noreply.github.com>
2023-10-20 16:55:28 -0300

    add jwt

--- src/main/java/com/example/demo/controller/AutenticacaoController.java
+++ src/main/java/com/example/demo/controller/AutenticacaoController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.usuario.Usuario;
 import com.example.demo.domain.usuario.UsuarioDTO;
+import com.example.demo.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
 
     @Autowired
+    private TokenService tokenService;
+
+    @Autowired
     private AuthenticationManager authenticationManager;
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid UsuarioDTO dados){
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var auth = authenticationManager.authenticate(token);
+        var tokenGerado = tokenService.gerarToken((Usuario) auth.getPrincipal());
         return ResponseEntity.ok().build();
     }
 }
